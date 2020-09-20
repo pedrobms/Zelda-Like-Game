f71bcb2904687fbf3aa9818f1ee07c23fd080876
Pedro Bilar Montero <pedro.bilar.montero@gmail.com>
2020-09-20 16:44:43 -0300

    Sign and dialog implemented
    Player UI
     - Image holder for dialog box and text
    Interactable script
     - Base script for interacting with objects
     - Knows when the player is in range (object needs a second collider as trigger)
     - Can display a simple message
    Sign script
     - Inherits from Interactable
     - Centers the display text
    
    Sign prefab created

---
+++ Assets/Scripts/InterectableObjects/Interactable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour{
+
+  public bool isPlayerInRange;
+  public GameObject dialogBox;
+  public Text dialogText;
+  public string dialog;
+
+  private PlayerStateManager stateManager;
+
+  public virtual void Update(){
+    if(Input.GetButtonDown("Interact") && isPlayerInRange && dialogBox != null){
+      stateManager.SetCurrentState(PlayerState.interact);
+      if(dialogBox.activeInHierarchy){
+        stateManager.SetCurrentState(PlayerState.idle);
+        dialogBox.SetActive(false);
+      }else{
+        dialogBox.SetActive(true);
+        dialogText.text = dialog;
+      }
+    }
+  }
+
+  public virtual void OnTriggerEnter2D(Collider2D other){
+    if(other.CompareTag("Player") && !other.isTrigger){      
+      stateManager = other.GetComponent<PlayerStateManager>();
+      isPlayerInRange = true;
+    }
+  }
+
+  public virtual void OnTriggerExit2D(Collider2D other){
+    if(other.CompareTag("Player") && !other.isTrigger){
+      isPlayerInRange = false;
+    }
+  }
+
+}
---
+++ Assets/Scripts/InterectableObjects/Sign.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : Interactable{
+
+  public override void OnTriggerEnter2D(Collider2D other){
+    base.OnTriggerEnter2D(other);
+    if(other.CompareTag("Player") && !other.isTrigger){
+      dialogText.alignment = TextAnchor.MiddleCenter;
+    }
+  }
+
+  public override void OnTriggerExit2D(Collider2D other){
+    base.OnTriggerExit2D(other);
+    if(other.CompareTag("Player") && !other.isTrigger){
+      dialogText.alignment = TextAnchor.UpperLeft;
+    }
+  }
+
+}
