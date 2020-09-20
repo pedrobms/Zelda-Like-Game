5fb8ee4d3746c82f21f9c3abe7d13ae3ee18ddfc
Pedro Bilar Montero <pedro.bilar.montero@gmail.com>
2020-09-20 17:52:50 -0300

    Player attack started
    Player animation
     - Sword attack animation added
    Player Attack script
     - Get input from player and play attack animation

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour{
+
+  private Animator anim;
+  private PlayerMovement player;
+  private PlayerStateManager stateManager;
+
+  void Start(){
+    anim = GetComponent<Animator>();
+    player = GetComponent<PlayerMovement>();
+    stateManager = GetComponent<PlayerStateManager>();
+  }
+
+  void Update(){
+    if(Input.GetButtonDown("Attack") && stateManager.GetCurrentState() != PlayerState.attack){
+      StartCoroutine(AttackCo());
+    }
+  }
+
+  private IEnumerator AttackCo(){
+    anim.SetBool("isAttacking", true);
+    stateManager.SetCurrentState(PlayerState.attack);
+    yield return null;
+    anim.SetBool("isAttacking", false);
+    yield return new WaitForSeconds(.3f);
+    stateManager.SetCurrentState(PlayerState.idle);
+  }
+}
